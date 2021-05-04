fcad139876009980881a756392ba020c9aeed7a6
plutownium <rolandmackintosh@outlook.com>
2021-05-03 20:13:05 -0700

    checkIfTheLikeCausedAMatch is written; updated docs; made new zombie code!

--- backend/carousel/carousel.js
+++ backend/carousel/carousel.js
@@ -1,31 +1,36 @@
-// generates the list of potential matches to display to the browsing user.
-// Because this is new, do it simply: Go into the db, pull out 5 potential matches, and
-// mark the time at which they were loaded. The request can be made for "5 matches that were seen least recently".
-// That way, the db pulls matches in chronological order.
+// // generates the list of potential matches to display to the browsing user.
+// // Because this is new, do it simply: Go into the db, pull out 5 potential matches, and
+// // mark the time at which they were loaded. The request can be made for "5 matches that were seen least recently".
+// // That way, the db pulls matches in chronological order.
 
-// Later, we can try to get fancy...
-const express = require("express");
-const router = express.Router();
+// // Later, we can try to get fancy...
+// const express = require("express");
+// const router = express.Router();
 
-const authorize = require("../_middleware/authorize");
-const db = require("../_helpers/db");
+// const authorize = require("../_middleware/authorize");
+// const db = require("../_helpers/db");
 
-module.exports = router;
+// module.exports = router;
 
-router.get("/potentialMatches", authorize(), (req, res, next) => {
-    const fetchingMatchesForUser = req.query.username;
+// router.get("/potentialMatches", authorize(), (req, res, next) => {
+//     const fetchingMatchesForUser = req.query.username;
 
-    // get list of users the currentUser has already liked.
-    const alreadyLikedUsers = db.User.findOne({ fetchingMatchesForUser }).likes;  // might use this later
-    // get candidates; that is, users who currentUser has not liked nor matched yet.
-    // TODO: integrate searching lastSeenHistory, sort & get the top 5 (as opposed to getting all, which is what we r doing now)
-    const candidates = await db.Match.find({ likedBy: { $nin: alreadyLikedUsers } });
-    const profiles = [];
+//     // get list of users the currentUser has already liked.
+//     const alreadyLikedUsers = db.User.findOne({ fetchingMatchesForUser }).likes;  // might use this later
+//     // get candidates; that is, users who currentUser has not liked nor matched yet.
+//     // TODO: integrate searching lastSeenHistory, sort & get the top 5 (as opposed to getting all, which is what we r doing now)
+//     const candidates = await db.Match.find({ likedBy: { $nin: alreadyLikedUsers } });
+//     const profiles = [];
 
-    for (let i = 0; i < candidates.length <= 5 ? candidates.length : 5; i++) {
-        const profile = await db.User.findOne({ username: candidates[i].username })
-        profiles.push(getPublicProfileInfo(profile))
-    }
-    // send the 5 profiles we picked
-    res.json(profiles)
-});
+//     for (let i = 0; i < candidates.length <= 5 ? candidates.length : 5; i++) {
+//         const profile = await db.User.findOne({ username: candidates[i].username })
+//         profiles.push(getPublicProfileInfo(profile))
+//     }
+//     // send the 5 profiles we picked
+//     res.json(profiles)
+// });
+
+// *** *** *** HEY LOOK *** *** ***
+// i have no idea what I was doing when I wrote this, like I can guess, but it was made 2 months ago.
+// rn my plan is to make a "Match" db and store matches as docs ... or .. ahhh what to do...
+// *** *** *** HEY LOOK *** *** ***
--- backend/controllers/matchmaker.js
+++ backend/controllers/matchmaker.js
@@ -19,14 +19,25 @@ module.exports = {
         const result = UserModel.updateLikesListForUser(
             req.params.suitorId,
             req.params.candidateId
+        ); // todo: do I need to do anything with the result? if this todo is still here past may 30, delete.
+        const theUsersMatched = checkIfTheLikeCausedAMatch(
+            suitorId, // he who did the liking
+            candidateIdToTest // he who is being tested: "does this person already like the liker?"
         );
-
-        const isMatch = checkIfMatch(u1, u2);
-        if (isMatch) {
+        if (theUsersMatched) {
             matchUsers(u1, u2);
-            // send congrats to both paties
+            res.status(200).send({
+                success: true,
+                message: "Congrats, you got a match!",
+                likedCausedMatch: true, // offer initiation of a chatroom
+            });
         } else {
             // tell frontend to make sparkles and move onto the next candidate
+            res.status(200).send({
+                success: true, // true as in "successful database operation," not "successful match"
+                message: "",
+                likedCausedMatch: false,
+            });
         }
     },
     onPass: async (req, res) => {
@@ -41,3 +52,15 @@ module.exports = {
         });
     },
 };
+
+function checkIfTheLikeCausedAMatch(suitorId, candidateIdToTest) {
+    const testTarget = UserModel.getUserById(candidateIdToTest);
+    const candidateAlreadyLikesSuitor = testTarget.likes.includes(suitorId);
+    if (candidateAlreadyLikesSuitor) {
+        return true;
+    } else {
+        return false; // no match!
+    }
+}
+
+function matchUsers(u1, u2) {}
